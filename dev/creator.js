8d66cff1ce0bcc7eeffba4be28d2a0641df31e1f
nthuTitan <43961291+nthuTitan@users.noreply.github.com>
2020-07-20 12:30:02 +0800

    merging into one file with different prototypes

--- dev/creator.js
+++ dev/creator.js
@@ -1,15 +1,62 @@
-const mappingTable = require("./mapping_table.js");
-const CreatorVerify = require("./creator_verify.js");
-const CreatorCreate = require("./creator_create.js");
+const vt = require("./voter.js");
 
-var newMappingTable;
-
-function Creator(ID){
-    if(!CreatorVerify(ID, mappingTable)){
-        console.log("CreatorVerify failed!");
-        return -1;
-    }
-    newMappingTable = CreatorCreate(ID, mappingTable);
+function Creator(ID, mappingTable, pendingTxPool){
+    this.creator = -1;
+    this.isCreatorVerified = -1;
+    this.newMappingTable = mappingTable;
+    this.pendingTxs = pendingTxPool.get_transaction();
+    this.isNewMappingTableVoted = -1;
 }
 
-module.exports = newMappingTable;
+Creator.prototype.CreatorVerify = function(ID, mappingTable) {
+    this.creator = -1;
+    this.isCreatorVerified = -1;
+
+    if(mappingTable.account == null){
+        console.log("Mapping Table is not valid!");
+        return -1;
+    }
+
+    for(var i = 0; i < mappingTable.numOfAddress; i++){
+        if(mappingTable.account[i].creator_bit == 1 && mappingTable.account[i].address == ID){
+            this.creator = ID;
+            this.isCreatorVerified = 1;
+            return 1;
+        }
+    }
+
+    console.log("Creator error!");
+    return -1;
+}
+
+Creator.prototype.CreatorCreate = function(ID) {
+    this.isNewMappingTableVoted = -1;
+
+    for(var i = 0; i < this.pendingTxs.length; i++){
+        if(this.newMappingTable.account.address == pendingTxs[i].sender){
+            this.newMappingTable.account.transactions.push(pendingTxs[i]);
+            this.newMappingTable.account.balance -= pendingTxs[i].value;
+        }
+        if(this.newMappingTable.account.address == pendingTxs[i].receiver){
+            this.newMappingTable.account.transactions.push(pendingTxs[i]);
+            this.newMappingTable.account.balance += pendingTxs[i].value;
+        }
+    }
+
+    return 1;
+}
+
+Creator.prototype.CreatorVoter = function(ID, mappingTable) {
+    this.isNewMappingTableVoted = -1;
+    var Voter = new vt(ID, this.newMappingTable);
+    
+    if(Voter.Vote() == true){
+        console.log("Voting successed!");
+        return this.newMappingTable;
+    }
+
+    console.log("Voting failed!");
+    return null;
+}
+
+module.exports = Creator;
--- dev/transaction_for_mapping_table.js
+++ dev/transaction_for_mapping_table.js
@@ -1,10 +1,27 @@
-const sha256 = require('sha256')
+// data structure for single transaction
+const sha256 = require('sha256');
 
 function Transaction_MT(id, sender, receiver, value) {
     this.id = id;
     this.sender = sender;
     this.receiver = receiver;
     this.value = value;
+}
+
+Transaction_MT.prototype.get_id = function() {
+    return this.id;
+}
+
+Transaction_MT.prototype.get_sender = function() {
+    return this.sender;
+}
+
+Transaction_MT.prototype.get_receiver = function() {
+    return this.receiver;
+}
+
+Transaction_MT.prototype.get_value = function() {
+    return this.value;
 }
 
 module.exports = Transaction_MT;
