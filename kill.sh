d86186167995736602fa8dac106c96b706d302b0
yoyolee118 <yoyolee118@hotmail.com>
2021-03-08 11:04:12 +0800

    created wallet jsdoc

--- kill.sh
+++ kill.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-killall /usr/local/lib/nodejs/node-v12.18.0-linux-x64/bin/node
-rm out*
-
 #!/usr/bin/bash
-# killall /usr/bin/node
-# rm out*
+ killall /usr/bin/node
+ rm out*
+
+#!/bin/bash
+#killall /usr/local/lib/nodejs/node-v12.18.0-linux-x64/bin/node
+#rm out*
 
 
