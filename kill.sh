eb67c9f921d54584e297e417d3831d4f1cdd4d01
GSLiTW <coslove8866@gmail.com>
2022-05-05 13:34:25 +0800

    chore: simplify kill.sh

--- kill.sh
+++ kill.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-kill $(lsof -t -i:3000)
-kill $(lsof -t -i:3001)
-kill $(lsof -t -i:3002)
-kill $(lsof -t -i:3003)
-kill $(lsof -t -i:3004)
-kill $(lsof -t -i:3005)
-kill $(lsof -t -i:3006)
-kill $(lsof -t -i:3007)
-kill $(lsof -t -i:3008)
+
+for i in {3000..3008}
+do
+    echo "Killing port $i"
+    kill $(lsof -t -i:$i)
+done
+
 killall /usr/local/bin/node
 rm out*
  
