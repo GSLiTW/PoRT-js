b87775254afd15e08452e215baa3ed4ca5133ead
GSLiTW <coslove8866@gmail.com>
2022-05-05 11:06:41 +0800

    chore: new kill.sh & delete node_modules folder

--- kill.sh
+++ kill.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
- killall /usr/local/bin/node
- rm out*
+kill $(lsof -t -i:3000)
+kill $(lsof -t -i:3001)
+kill $(lsof -t -i:3002)
+kill $(lsof -t -i:3003)
+kill $(lsof -t -i:3004)
+kill $(lsof -t -i:3005)
+kill $(lsof -t -i:3006)
+kill $(lsof -t -i:3007)
+kill $(lsof -t -i:3008)
+killall /usr/local/bin/node
+rm out*
  
