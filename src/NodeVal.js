01fc8c8e89ed994d99c56a5d4f19aaaea80c7685
peterchen999 <peter.chen999@gmail.com>
2022-06-18 17:05:01 +0800

    chore - rename NodeVal.value to NodeVal.balance to avoid misunderstanding

--- src/NodeVal.js
+++ src/NodeVal.js
@@ -1,18 +1,18 @@
 /**
  * Constructor of the NodeVal Class
  * @class Data Structure for value of MPT node
- * @param  {Number} [value=0]
+ * @param  {Number} [balance=0]
  * @param  {Number} [tax=0]
  * @param  {integer={0, 1, 2}} [Dbit=0]
  */
-function NodeVal(value = 0, tax = 0, DirtyBit = 0) {
-    this.value = value;
+function NodeVal(balance = 0, tax = 0, DirtyBit = 0) {
+    this.balance = balance;
     this.tax = tax;
     this.DirtyBit = DirtyBit;
 };
 
-NodeVal.prototype.Value = function() {
-    return this.value;
+NodeVal.prototype.Balance = function() {
+    return this.balance;
 };
 
 NodeVal.prototype.Tax = function() {
@@ -23,8 +23,8 @@ NodeVal.prototype.Dbit = function() {
     return this.DirtyBit;
 };
 
-NodeVal.prototype.SetValue = function(value) {
-    this.value = value;
+NodeVal.prototype.SetBalance = function(balance) {
+    this.balance = balance;
 };
 
 NodeVal.prototype.SetTax = function(tax) {
@@ -34,5 +34,11 @@ NodeVal.prototype.SetTax = function(tax) {
 NodeVal.prototype.SetDbit = function(Dbit) {
     this.DirtyBit = Dbit;
 };
+
+NodeVal.prototype.SetValue = function(balance, tax, Dbit) {
+    this.balance = balance;
+    this.tax = tax;
+    this.DirtyBit = Dbit;
+}
 
 module.exports = NodeVal;
--- test/NodeVal.test.js
+++ test/NodeVal.test.js
@@ -2,17 +2,21 @@ const NodeVal = require('../src/NodeVal')
 
 test('constructor', () => {
     let testingNode = new NodeVal(15, 7, 1)
-    expect(testingNode.Value()).toEqual(15);
+    expect(testingNode.Balance()).toEqual(15);
     expect(testingNode.Tax()).toEqual(7);
     expect(testingNode.Dbit()).toEqual(1);
 });
 
 test('setter', () => {
     let testingNode = new NodeVal()
-    testingNode.SetValue(15)
+    testingNode.SetBalance(15)
     testingNode.SetTax(7)
     testingNode.SetDbit(1)
-    expect(testingNode.Value()).toEqual(15);
+    expect(testingNode.Balance()).toEqual(15);
     expect(testingNode.Tax()).toEqual(7);
     expect(testingNode.Dbit()).toEqual(1);
+    testingNode.SetValue(2, 1, 0);
+    expect(testingNode.Balance()).toEqual(2);
+    expect(testingNode.Tax()).toEqual(1);
+    expect(testingNode.Dbit()).toEqual(0);
 });
