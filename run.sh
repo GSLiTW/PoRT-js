e4e681e57cfc2d84b47a46089e0f43c6bee00fda
xyhy <xyhy108062213@gapp.nthu.edu.tw>
2022-09-02 23:03:07 +0800

    fix: delete *.out and revert to for-loop run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-npm run 3000 > out3000 &
-npm run 3001 > out3001 &
-npm run 3002 > out3002 &
-npm run 3003 > out3003 &
-npm run 3004 > out3004 &
-npm run 3005 > out3005 &
-npm run 3006 > out3006 &
-npm run 3007 > out3007 &
-npm run 3008 > out3008 
+for i in {3000..3008}
+ do
+     echo "Starting port $i"
+     npm run $i > $i.out 
+ done
