b6c57c959977af903ba29bf22a4e981f24a280a7
xyhy <xyhy108062213@gapp.nthu.edu.tw>
2022-09-23 16:30:16 +0800

    fix: fix genesisData readsync

--- src/Block/blockchain.js
+++ src/Block/blockchain.js
@@ -28,29 +28,24 @@ function Blockchain(MPT) {
   const txn_pool = new Txn_Pool();
   txn_pool.create(1, MPT);
 
+  let genesisData;
+  const dataFile = fs.readFileSync('./src/Block/genesisBlock.json');
+  try {
+    genesisData = JSON.parse(dataFile);
+    console.log('JSON string:', 'utf8', genesisData);
+  } catch (err) {
+    console.log('Error parsing JSON string:', err);
+  }
   const genesisBlock = new Block(
       1, // height
       txn_pool.transactions,
       0, // previous Hash
       MPT,
   );
-
-  const f = fs.readFile('./src/Block/genesisBlock.json', (err, data) => {
-    if (err) {
-      return console.log('Error reading file from disk:', err);
-    }
-    try {
-      const genesisData = JSON.parse(data);
-      console.log(genesisData);
-      genesisBlock.timestamp = genesisData.timestamp;
-      genesisBlock.hash = genesisData.hash;
-      genesisBlock.nextCreator = genesisData.nextCreator;
-      genesisBlock.nextVoters = genesisData.nextVoters;
-    } catch (err) {
-      console.log('Error parsing JSON string:', err);
-    }
-  });
-
+  genesisBlock.timestamp = genesisData.timestamp;
+  genesisBlock.hash = genesisData.hash;
+  genesisBlock.nextCreator = genesisData.nextCreator;
+  genesisBlock.nextVoters = genesisData.nextVoters;
   this.chain.push(genesisBlock); // create Genesis Block
 }
 
