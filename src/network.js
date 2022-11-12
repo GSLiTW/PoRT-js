c18a832ce3dd92afbe1c2092f00af0fc4afa85e7
jason7580 <jason73680@gmail.com>
2022-11-12 14:25:32 +0800

    merge with new commits

--- src/network.js
+++ src/network.js
@@ -46,79 +46,7 @@ const chain = init_data.chain;
 const Tree = init_data.tree;
 const pending_txn_pool=init_data.pending_txn_pool;
 const wallet=init_data.wallet;
-*/
 
-const data = fs.readFileSync('./data/node_address_mapping_table.csv')
-    .toString() // convert Buffer to string
-    .split('\n') // split string to lines
-    .map((e) => e.trim()) // remove white spaces for each line
-    .map((e) => e.split(',').map((e) => e.trim())); // split each line to array
-
-let w = fs.readFileSync('./data/private_public_key.csv')
-    .toString() // convert Buffer to string
-    .split('\n') // split string to lines
-    .map((e) => e.trim()) // remove white spaces for each line
-    .map((e) => e.split(',').map((e) => e.trim())); // split each line to array
-const wallet = new Wallet(w[port - 3000][1], w[port - 3000][2], 10);
-w = undefined;
-//createtxs(1)
-
-
-/*
-for (let i = 0; i < 14; i++) {
-  if (i == 2) Tree.Insert(data[i][2], 100 * BASE, 1 * BASE * 0.0001, [2, 1]); // dbit == 1 means creator
-  else if (i == 4) Tree.Insert(data[i][2], 1 * BASE, 1 * BASE * 0.0001, [2, 2]); // dbit == 2 means voter
-  else if (i == 6) Tree.Insert(data[i][2], 1 * BASE, 1 * BASE * 0.0001, [2, 2]); // dbit == 2 means voter
-  else if (i == 8) Tree.Insert(data[i][2], 1 * BASE, 1 * BASE * 0.0001, [2, 2]); // dbit == 2 means voter
-  else Tree.Insert(data[i][2], 1 * BASE, 1 * BASE * 0.0001, [0, 0]);
-}
-*/
-let chain = new Blockchain();
-const Tree = chain.MPT;
-
-function insertCSVData(quantity, data) {
-  txns = [];
-  for (let i = 1; i <= quantity; i++) {
-    if(data[i][2] === wallet.publicKey.encode('hex')){
-      const sig = wallet.Sign(data[i][0])
-      const newTx = new Transaction(data[i][0], data[i][2], data[i][3], data[i][4], sig, chain.MPT)
-      //storeData(newTx, `./${port}.json`)
-      const requestPromises = [];
-      // console.log(chain.networkNodes);
-      console.log("to tx broadcast");
-      chain.networkNodes.forEach((networkNodeUrl) => {
-        const requestOptions = {
-          uri: networkNodeUrl + '/transaction/broadcast',
-          method: 'POST',
-          body: {NewTxs: newTx},
-          json: true,
-          retry: 2,
-          delay: 10000,
-        };
-  
-        requestPromises.push(rp(requestOptions));
-      });
-  
-      Promise.all(requestPromises).then((data) => {
-        console.log('Transaction created and broadcast successfully.');
-      });
-    }
-
-  }
-  return null;
-};
-
-function createtxs(num) {
-  const csvdata = new CSV_data();
-  const data_ = csvdata.getData(num); // get data of block1
-  if (num == 1 || num == 2) {
-    console.log("add txn");
-    return insertCSVData(4, data_);
-  } else if (num == 3) {
-    console.log("add txn2");
-    return insertCSVData(4, data_);
-  } else console.log("wrong block number.");
-}
 
 // if(port != 3000){
 //   const newNodeUrl = 'http://localhost:' + '3000';
