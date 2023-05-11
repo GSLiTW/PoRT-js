809d044c8a7a1c69165d56182e645b85e9158ff2
xyhy <xyhy108062213@gapp.nthu.edu.tw>
2023-05-11 15:27:36 +0800

    doc: cosig doc finish

--- src/cosig.js
+++ src/cosig.js
@@ -5,7 +5,7 @@ const hashVerify = crypto.createHash('sha256');
 const BN = require('bn.js');
 
 /**
- * Cosig class is responsible for do cosig algorithm calculation
+ * Cosig class is a library which is responsible for doing cosig algorithm calculation
  * @class
  * @param  {number} challenge - creator generate challenge
  * @param  {number} r0 - gather all response from voter and calculate final result
@@ -16,10 +16,10 @@ function Cosig() {
 }
 
 /**
- *
- * @param {*} voterPubV - string of all voter PublicV
+ * Creator generate challenge in normal state
+ * @param {Array} voterPubV - string of all voter PublicV
  * @param {Block} block - whole new Block
- * @return {string} - challenge(hex type)
+ * @return {string} - hex string of challenge
  */
 Cosig.prototype.generateChallenge = function(voterPubV, block) {
   this.v0Aggr = voterPubV[0];
@@ -33,6 +33,14 @@ Cosig.prototype.generateChallenge = function(voterPubV, block) {
   return this.challenge;
 };
 
+/**
+ * Creator generate challenge in the state of any voter has disconnected
+ * @param {Array} voterPubV - string of all voter PublicV
+ * @param {Block} block - whole new Block
+ * @param {Array} voterIndex - index of voter who has attended
+ * @returns {string} - hex string of challenge
+ */
+
 Cosig.prototype.generateChallengeWithIndex = function(voterPubV, block, voterIndex) {
   this.v0Aggr = voterPubV[voterIndex[0]];
   for (let i = 1; i < voterIndex.length; i++) {
@@ -45,6 +53,11 @@ Cosig.prototype.generateChallengeWithIndex = function(voterPubV, block, voterInd
   return this.challenge;
 };
 
+/**
+ * Creator generate response
+ * @param {Array} voterResponse - response from voters
+ * @returns {string} - response
+ */
 Cosig.prototype.aggregateResponse = function(voterResponse) {
   this.r0Aggr = voterResponse[0];
   for (let i = 1; i < voterResponse.length; i++) {
@@ -54,6 +67,14 @@ Cosig.prototype.aggregateResponse = function(voterResponse) {
   return this.r0Aggr;
 };
 
+/**
+ * Verify cosig by comparing challenges computed in two different methods
+ * @param {string} gr0 - publick key generated with ECDSA by utilizing r0Aggr as private key
+ * @param {string} x0c - compute with voter's public keys and challenge
+ * @param {string} challenge - hex string of challenge
+ * @param {Block} block - whole new Block
+ * @returns {boolean}
+ */
 Cosig.prototype.verifyCosig = function(gr0, x0c, challenge, block) {
   const newpubV = gr0.add(x0c);
   hashVerify.update(newpubV.encode('hex') + block);
@@ -61,7 +82,12 @@ Cosig.prototype.verifyCosig = function(gr0, x0c, challenge, block) {
   const result = newchallenge.eq(challenge);
   return result;
 };
-
+/**
+ * Compute x0 with voter's public keys and challenge
+ * @param {Array} voterPubKey - hex string of all voter's public keys
+ * @param {string} challenge - hex string of challenge
+ * @returns {string}
+ */
 Cosig.prototype.computePubkeyMulWithChallenge = function(voterPubKey, challenge) {
   let x0 = voterPubKey[0];
   for (let i = 1; i < voterPubKey.length; i++) {
@@ -70,7 +96,13 @@ Cosig.prototype.computePubkeyMulWithChallenge = function(voterPubKey, challenge)
   x0 = x0.mul(challenge);
   return x0;
 };
-
+/**
+ * Voter generate response
+ * @param {string} cHex - hex string of challenge
+ * @param {string} secretv - voter secretv
+ * @param {string} privateKey - voter private key
+ * @returns {string} - hex string of response
+ */
 Cosig.prototype.GenerateResponse = function(cHex, secretv, privateKey) {
   const c = new BN(cHex, 'hex');
   const v = new BN(secretv.toString('hex'), 'hex');
